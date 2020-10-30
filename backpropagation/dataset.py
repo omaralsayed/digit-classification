74a4ad68f76139331fba61573c6b93ea196b5d4f
Omar Alsayed <42531544+omaralsayed@users.noreply.github.com>
2020-10-30 02:37:15 -0400

    Refactor parser

--- backpropagation/dataset.py
+++ backpropagation/dataset.py
@@ -10,14 +10,14 @@ the respective image (a value between 0 and 9).
 import random
 import numpy as np
 
-class MNIST:
+class MNIST():
     x_train = [None] * 4000
     y_train = [None] * 4000
     x_test  = [None] * 1000
     y_test  = [None] * 1000
 
-def mnist():
-    m = MNIST()
+def get_sets():
+    sets = MNIST()
 
     mnist_images = open('MNISTnumImages5000_balanced.txt')
     mnist_labels = open('MNISTnumLabels5000_balanced.txt')
@@ -25,55 +25,55 @@ def mnist():
     image_file = mnist_images.readlines()
     label_file = mnist_labels.readlines()
 
-    images = []
+    images = list()
     for line in image_file:
         images.append(line.split())
 
-    labels = []
+    labels = list()
     for line in label_file:
         labels.append(line.strip())
 
-    m.x_train = (images[:400]+ images[499:899]   + images[999:1399] 
-        + images[1499:1899]  + images[1999:2399] + images[2499:2899]
-        + images[2999:3399]  + images[3499:3899] + images[3999:4399]
+    sets.x_train = (images[:400] + images[499:899]   + images[999:1399] 
+        + images[1499:1899]      + images[1999:2399] + images[2499:2899]
+        + images[2999:3399]      + images[3499:3899] + images[3999:4399]
         + images[4499:4899])
 
-    m.y_train = (labels[:400]+ labels[499:899]   + labels[999:1399]  
-        + labels[1499:1899]  + labels[1999:2399] + labels[2499:2899] 
-        + labels[2999:3399]  + labels[3499:3899] + labels[3999:4399]
+    sets.y_train = (labels[:400] + labels[499:899]   + labels[999:1399]  
+        + labels[1499:1899]      + labels[1999:2399] + labels[2499:2899] 
+        + labels[2999:3399]      + labels[3499:3899] + labels[3999:4399]
         + labels[4499:4899])
 
-    data = list(zip(m.x_train, m.y_train))
+    data = list(zip(sets.x_train, sets.y_train))
     random.shuffle(data) # Shuffle training set
-    m.x_train, m.y_train = zip(*data)
+    sets.x_train, sets.y_train = zip(*data)
 
-    m.x_train = np.array(m.x_train).astype('float32')
-    m.y_train = np.array(m.y_train).astype(int)
+    sets.x_train = np.array(sets.x_train).astype('float32')
+    sets.y_train = np.array(sets.y_train).astype(int)
 
-    m.x_test = (images[399:499]+ images[899:999]   + images[1399:1499] 
-        + images[1899:1999]    + images[2399:2499] + images[2899:2999] 
-        + images[3399:3499]    + images[3899:3999] + images[4399:4499] 
+    sets.x_test = (images[399:499] + images[899:999]   + images[1399:1499] 
+        + images[1899:1999]        + images[2399:2499] + images[2899:2999] 
+        + images[3399:3499]        + images[3899:3999] + images[4399:4499] 
         + images[4899:4999])
 
-    m.y_test = (labels[399:499]+ labels[899:999]   + labels[1399:1499] 
-        + labels[1899:1999]    + labels[2399:2499] + labels[2899:2999] 
-        + labels[3399:3499]    + labels[3899:3999] + labels[4399:4499] 
+    sets.y_test = (labels[399:499] + labels[899:999]   + labels[1399:1499] 
+        + labels[1899:1999]        + labels[2399:2499] + labels[2899:2999] 
+        + labels[3399:3499]        + labels[3899:3999] + labels[4399:4499] 
         + labels[4899:4999])
 
-    data = list(zip(m.x_test, m.y_test))
+    data = list(zip(sets.x_test, sets.y_test))
     random.shuffle(data) # Shuffle testing set
-    m.x_test, m.y_test = zip(*data)
+    sets.x_test, sets.y_test = zip(*data)
 
-    m.x_test = np.array(m.x_test).astype('float32')
-    m.y_test = np.array(m.y_test).astype(int)
+    sets.x_test = np.array(sets.x_test).astype('float32')
+    sets.y_test = np.array(sets.y_test).astype(int)
 
-    m.y_train = encode(10, m.y_train)
-    m.y_test  = encode(10, m.y_test)
+    sets.y_train = encode(10, sets.y_train)
+    sets.y_test  = encode(10, sets.y_test)
 
     mnist_images.close()
     mnist_labels.close()
 
-    return m
+    return sets
 
 # One-hot encoding
 def encode(digits, labels):
